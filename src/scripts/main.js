9e0f3a076a78735f9e21a03f62cafad20141ba91
Kato Miku <num02akakatoh@gmail.com>
2023-04-12 00:43:50 +0900

    Update: 2023/04/09更新分
    サイト完成

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,80 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hero = new HeroSlider(".swiper");
-  hero.start();
+class Main {
+  constructor() {
+    this.header = document.querySelector(".header");
+    this.hero = new HeroSlider(".swiper");
+    this.sides = document.querySelectorAll(".side");
+    this._observers = [];
+    this._init();
+  }
 
-  const _textAnimation = function (el, inview) {
+  _init() {
+    new MobileMenu();
+    Pace.on("done", this._scrollInit.bind(this));
+  }
+
+  destroy() {
+    this._observers.forEach((so) => so.destroy());
+  }
+
+  _toggleSlideAnimation(el, inview) {
     if (inview) {
-      const ta = new TweenTextAnimation(el);
-      ta.animate();
+      this.hero.start();
+    } else {
+      this.hero.stop();
     }
-  };
+  }
 
-  const so = new ScrollObserver(".tween-animate-title", _textAnimation);
+  _scrollInit() {
+    this._observers.push(
+      new ScrollObserver(".nav-trigger", this._navAnimation.bind(this), {
+        once: false,
+      }),
+      new ScrollObserver(".swiper", this._toggleSlideAnimation.bind(this), {
+        once: false,
+      }),
+      new ScrollObserver("#main-content", this._sideAnimation.bind(this), {
+        once: false,
+        rootMargin: "-300px 0px",
+      }),
+      new ScrollObserver(".cover-slide", this._inviewAnimation),
+      new ScrollObserver(".appear", this._inviewAnimation),
+      new ScrollObserver(".tween-animate-title", this._textAnimation)
+    );
+  }
 
-  const _inviewAnimation = function (el, inview) {
+  _navAnimation(el, inview) {
+    if (inview) {
+      this.header.classList.remove("triggerd");
+    } else {
+      this.header.classList.add("triggerd");
+    }
+  }
+
+  _sideAnimation(el, inview) {
+    if (inview) {
+      this.sides.forEach((side) => {
+        side.classList.add("inview");
+      });
+    } else {
+      this.sides.forEach((side) => {
+        side.classList.remove("inview");
+      });
+    }
+  }
+
+  _inviewAnimation(el, inview) {
     if (inview) {
       el.classList.add("inview");
     } else {
       el.classList.remove("inview");
     }
-  };
+  }
 
-  const so2 = new ScrollObserver(".cover-slide", _inviewAnimation);
-});
+  _textAnimation(el, inview) {
+    if (inview) {
+      const ta = new TweenTextAnimation(el);
+      ta.animate();
+    }
+  }
+}
+new Main();
